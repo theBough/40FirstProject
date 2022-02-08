1c32548031376dfb9b07fbb710b75d7628c7ca83
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-08 11:36:48 -0600

    Create wall.js

---
+++ wall.js
@@ -0,0 +1,13 @@
+function Wall(x,y,w,h,col){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
+  
+  
+  this.display = function(){
+    fill(this.col)
+    rect(this.x, this.y, this.w, this.h);
+  }//end display
+}//end Wall
