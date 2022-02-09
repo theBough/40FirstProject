b36af51419ef1a0aca386de5d55a4132f685efd6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-09 11:26:15 -0600

    This Commit has wall collission
    Be sure to add line 8.
    
    You might have to change all "walls" to "w"

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ function setup() {
   createCanvas(400, 400);
   p = new Player(200,200,10,10,"blue")
   createWalls();
+  checkForCollission();
 }
 
 function draw() {
@@ -20,3 +21,30 @@ function createWalls(){
   w.push(new Wall(100,100,300,20,"pink"))
   w.push(new Wall(100,300,300,20,"white"))
 }
+
+function checkForCollission() {
+  for (var i = 0; i < walls.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= walls[i].y + walls[i].h && p.y + p.h >= walls[i].y && p.x <= walls[i].x + walls[i].w && p.x >= walls[i].x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= walls[i].y + walls[i].h && p.y + p.h >= walls[i].y && p.x + p.w >= walls[i].x && p.x <= walls[i].x + walls[i].w) {
+      p.x -= 5
+    }
+
+
+    if (p.x <= walls[i].x + walls[i].w && p.x + p.w >= walls[i].x && p.y <= walls[i].y + walls[i].h && p.y >= walls[i].y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= walls[i].x + walls[i].w && p.x + p.w >= walls[i].x && p.y + p.h >= walls[i].y && p.y <= walls[i].y + walls[i].h) {
+      p.y -= 5;
+    }
+
+  }
+}
+
