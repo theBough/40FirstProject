491996445957c1f5985be9ec4d05425f7012ede8
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-09 12:06:02 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@ function setup() {
   createCanvas(400, 400);
   p = new Player(200,200,10,10,"blue")
   createWalls();
+  
 }
-
 function draw() {
   background(102,255,255);
   for(var i =0 ; i < w.length ; i++){
@@ -15,8 +15,17 @@ function draw() {
   
   p.display();
   p.update();
+  checkForRoomChange();
   checkForCollission();
 }
+function checkForRoomChange(){
+  //this is going to check if the player hits
+  //the edge of the canvas.
+  if(p.y < 0){
+    //The player has gone off the top of the canvas
+    p.y = height;
+  }//end if
+}//end checkForRoomChange
 function createWalls(){
   w.push(new Wall(100,100,300,20,"pink"))
   w.push(new Wall(100,300,300,20,"white"))
@@ -28,23 +37,23 @@ function checkForCollission() {
     //check if we hit the left of any wall
     if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
       p.x += 5
-    }//end if
+    }
 
     //check if we hit the right of any wall
     if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
       p.x -= 5
-    }//end if
+    }
 
 
     if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
       p.y += 5;
-    }//end if
+    }
 
     //check if we hit the top of any wall
     if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
       p.y -= 5;
-    }//end if
+    }
 
-  }//end for loop
-}//end function checkForCollission
+  }
+}
 
