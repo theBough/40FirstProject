cc55e8549f476dafa8b098171ec6d596fed0425e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-10 11:50:50 -0600

    Update rooms.js

--- rooms.js
+++ rooms.js
@@ -1,11 +1,11 @@
 function drawRoomOne(){
-   w.push(new Wall(100,100,300,20,"pink"))
+   w.push(new Wall(100,100,300,20,"red"))
   w.push(new Wall(100,300,300,20,"white"))
 }//end drawRoomOne
 
 function drawRoomTwo(){
-  
+   w.push(new Wall(100,100,300,20,"red"))
 }//end drawRoomTwo
-function drawRoomTwo(){
+function drawRoomThree(){
   
 }//end drawRoomTwo
