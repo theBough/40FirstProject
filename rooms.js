aa2012005e2eac9c6a0fbdf8e72cfbbabb42d58f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-15 11:10:09 -0600

    Update rooms.js
    I've added the WhereTo() and finished the whereTo, we shouldn't have to touch it again.

--- rooms.js
+++ rooms.js
@@ -11,3 +11,27 @@ function drawRoomTwo(){
 function drawRoomThree(){
   
 }//end drawRoomTwo
+function checkForRoomChange(){
+  //this is going to check if the player hits
+  //the edge of the canvas.
+  if(p.y + p.h < 0){
+    //The player has gone off the top of the canvas
+    p.y = height;
+    whereTo("up");
+  }//end if
+  if(p.y > height){
+    //The player has gone passt the bottom of the canvas
+    p.y = 0;
+    whereTo("down");
+  }//end if
+  if(p.x > width){
+    //The player has gone passt the right of the canvas
+    p.x = 0;
+    whereTo("right");
+  }//end if
+  if(p.x + p.w < 0){
+    //The player has gone passt the left of the canvas
+    p.x = width;
+    whereTo("left");
+  }//end if
+}//end checkForRoomChange
