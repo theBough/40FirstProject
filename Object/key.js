08e81746e5e0fe461231f1e11c39b1246858212d
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-18 11:17:03 -0600

    Update key.js

--- Object/key.js
+++ Object/key.js
@@ -1,1 +1,18 @@
-//test
+function Key(x,y,w,h){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  //This is a boolean that determines if the player
+  //has the key
+  this.have = false;
+  //this is a boolean that we will use to toggle
+  //the visibility on and off
+  this.show = false;
+  this.img = loadImage("object/key.png")
+  
+  this.display = function(){
+    image(this.img, this.x, this.y);
+  }//end display
+  
+}//end Key
