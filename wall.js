afa49f3dd9f0c46c59c230adc7fa80aab9d54062
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-18 11:31:20 -0600

    Update wall.js

--- wall.js
+++ wall.js
@@ -11,3 +11,31 @@ function Wall(x,y,w,h,col){
     rect(this.x, this.y, this.w, this.h);
   }//end display
 }//end Wall
+
+function checkForCollission() {
+  for (var i = 0; i < w.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
+      p.x -= 5
+    }
+
+
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
+      p.y -= 5;
+    }
+
+  }
+}
+
+
