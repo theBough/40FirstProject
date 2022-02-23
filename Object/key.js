7ff33bc1eb0de7d2283d69ef95f69794576cffd8
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-23 11:48:37 -0600

    Added collision

--- Object/key.js
+++ Object/key.js
@@ -1,8 +1,10 @@
-function Key(x, y, w, h) {
+function Key(x, y, w, h,r) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+  //r will be the room you want the key in.
+  this.r = r;
   //This is a boolean that determines if the player
   //has the key
   this.have = false;
@@ -10,9 +12,37 @@ function Key(x, y, w, h) {
   //the visibility on and off
   this.img = loadImage("object/key.png");
   this.display = function () {
-    if (room == 2) {
+    if (room == this.r) {
       this.img.resize(this.w, this.h);
       image(this.img, this.x, this.y);
     }//end if
   }; //end display
+  this.keyCollision = function() {
+  
+
+    //check if we hit the left of any wall
+    if (p.y <= this.y + this.h && p.y + p.h >= this.y && p.x <= this.x + this.w && p.x >= this.x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= this.y + this.h && p.y + p.h >= this.y && p.x + p.w >= this.x && p.x <= this.x + this.w) {
+      p.x -= 5
+    }
+
+
+    if (p.x <= this.x + this.w && p.x + p.w >= this.x && p.y <= this.y + this.h && p.y >= this.y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= this.x + this.w && p.x + p.w >= this.x && p.y + p.h >= this.y && p.y <= this.y + this.h) {
+      p.y -= 5;
+    }
+
+  }
+
+  
 } //end Key
+
+
