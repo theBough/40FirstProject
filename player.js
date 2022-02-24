3bc92b04579dea0f1094a804b63afa426efd1046
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-02-24 11:08:38 -0600

    The Key should Follow the player

--- player.js
+++ player.js
@@ -17,21 +17,39 @@ function Player(x,y,w,h,col){
     if(keyIsDown(39)){
       //this is the right arrow key
       this.x += this.speed;
+      direction="right"
+      if(k.have){
+        k.x = p.x+p.w;
+        k.y = p.y;
+      }
       
     }//end if
     if(keyIsDown(37)){
       //this is the left arrow key
+      direction="left"
       this.x -= this.speed;
+      if(k.have){
+        k.x = p.x - k.w;
+        k.y = p.y
+      }
       
     }//end if
      if(keyIsDown(38)){
       //this is the up arrow key
       this.y -= this.speed;
+       if(k.have){
+         k.x = p.x;
+         k.y = p.y - k.h
+       }
       
     }//end if
     if(keyIsDown(40)){
       //this is the down arrow key
       this.y += this.speed;
+      if(k.have){
+        k.x = p.x;
+        k.y = p.y + p.h
+      }
       
     }//end if
   }//end update
