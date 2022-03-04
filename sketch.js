cff0a53324dbfb72d3134303ad7881bbcba38ecd
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-04 10:51:58 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,40 +1,41 @@
 let x;
 let p;
 let w = [];
-let rooms=[];
+let rooms = [];
 let k;
 let gateOneClosed = true;
 let row, column;
 
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,10,10,"blue");
-  //k = new Key(300,250, 10,10,2);
-  
-  k = new Key(300,250, 10,10,2);
-  //set row and column to the starting point in 
-  //array, of your game
-  row = 0;
-  column =1;
   fillRooms();
+  p = new Player(200, 200, 10, 10, "blue");
+  
+  
+
+ k = new Key(300, 250, 10, 10, keyRoom);
+  //set row and column to the starting point in
+  //array, of your game
+  row = 1;
+  column = 1;
+  rooms[row][column].call();
+
+
 }
 function draw() {
   background("#db712a");
-  for(var i =0 ; i < w.length ; i++){
-    w[i].display()
+    
+  for (var i = 0; i < w.length; i++) {
+    w[i].display();
   }
   p.display();
   p.update();
   k.display();
   k.keyCollision();
-  //this is calling the function to draw a romm 
+  //this is calling the function to draw a romm
   //from the array 'rooms'
-  
+
   checkForRoomChange();
   rooms[row][column].call();
   checkForCollission();
 }
-
-
-
-
