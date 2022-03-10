53f5bf8e943d8c1e6a9c4447c2f5a799482a2218
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-03-10 11:33:32 -0600

    Added an Enemy

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//variable declarations
 let x;
 let p;
 let w = [];
@@ -5,22 +6,19 @@ let rooms = [];
 let k;
 let gateOneClosed = true;
 let row, column;
+let e;
 
 function setup() {
   createCanvas(400, 400);
   fillRooms();
   p = new Player(200, 200, 10, 10, "blue");
-  
-  
-
- k = new Key(300, 250, 10, 10, keyRoom);
+   k = new Key(100, 300, 10, 10, keyRoom);
+  e = new Enemy(200,200,20,20);
   //set row and column to the starting point in
   //array, of your game
   row = 1;
   column = 1;
   rooms[row][column].call();
-
-
 }
 function draw() {
   background("#db712a");
@@ -32,6 +30,7 @@ function draw() {
   p.update();
   k.display();
   k.keyCollision();
+  e.display();
   //this is calling the function to draw a romm
   //from the array 'rooms'
 
